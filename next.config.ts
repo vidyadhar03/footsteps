a60ff668ac3079d61d8881f04d7c32e67b38e61d
Vidyadhar Gowd <vidyadhariitkgp@gmail.com>
2025-11-19 05:25:24 +0530

    deeplink support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+      {
+        source: "/.well-known/assetlinks.json",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
